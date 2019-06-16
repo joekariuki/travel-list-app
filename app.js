050c27e168dafa5cf4cd8092bb7818d6cce3660c
Joe Kariuki <jknguru6@gmail.com>
2019-06-16 06:17:27 -0400

    Define structure & Add Travel class

---
+++ app.js
@@ -0,0 +1,20 @@
+
+//  Travel Class: Represents a trip
+class Travel {
+    constructor(location, arrivalDate, departureDate, tripDetails) {
+        this.location = location,
+        this.arrivalDate = arrivalDate,
+        this.departureDate = departureDate,
+        this.tripDetails = tripDetails,
+    }
+}
+// UI Class: Handle UI Tasks
+
+// Store Class: Handles Storage
+
+// Event: Display books
+
+
+// Event: Add a book
+
+// Event: Remove a book
